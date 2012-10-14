29b3ef9a1264b564d5d8aa5bd1837702875bd2e3
Martin Høst Normark <m@martinnormark.com>
2012-10-14 12:19:32 +0200

    Add description to assembly info

--- PreMailer.Net/PreMailer.Net/Properties/AssemblyInfo.cs
+++ PreMailer.Net/PreMailer.Net/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PreMailer.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Pre-process HTML e-mails by moving CSS inline for maximum e-mail client compatibility.")]
 [assembly: AssemblyCulture("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
