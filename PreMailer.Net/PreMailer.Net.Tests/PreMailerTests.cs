7f115440b0fa20d72d25c941026c37bde033e232
ShurikEv <shurikev@gmail.com>
2013-03-11 19:14:47 +0700

    update project to .NET 3.5 and implement inherit css styles from parent element

--- PreMailer.Net/PreMailer.Net.Tests/PreMailerTests.cs
+++ PreMailer.Net/PreMailer.Net.Tests/PreMailerTests.cs
@@ -38,6 +38,26 @@ namespace PreMailer.Net.Tests
 			Assert.IsTrue(premailedOutput.Contains("<div class=\"test\" style=\"width: 100px;"));
 		}
 
+        [TestMethod]
+        public void MoveCssInline_InlineStyleElementTakesPrecedence2()
+        {
+            string input = "<html><head><style type=\"text/css\">.test { width: 150px; } .my { color: red; } </style></head><body><div class=\"test\"><p class=\"my\">ff</p></div></body></html>";
+
+            string premailedOutput = sut.MoveCssInline(input, false);
+
+            Assert.IsTrue(premailedOutput.Contains("<div class=\"test\" style=\"width: 150px;\"><p class=\"my\" sytle=\"width: 150px;color=red;\""));
+        }
+
+        [TestMethod]
+        public void MoveCssInline_InlineStyleElementTakesPrecedence3()
+        {
+            string input = "<style type=\"text/css\">.test { width: 150px; } .my { color: red; } .test { width: 200px; font:bold; } </style><div class=\"test\"><p class=\"my\" style=\"color: green;\"><label style=\"color:black\">TEST</label></p></div>";
+
+            string premailedOutput = sut.MoveCssInline2(input, false);
+
+            Assert.IsTrue(premailedOutput.Contains("<div class=\"test\" style=\"width: 150px;\"><p class=\"my\" sytle=\"width: 150px;color=red;\""));
+        }
+
 		[TestMethod, Ignore]
 		public void ManualIntegrationTest()
 		{
