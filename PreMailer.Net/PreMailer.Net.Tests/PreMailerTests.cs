59728af6332a371775d98627697148df68b09c7d
Eric J. Smith <eric@codesmithtools.com>
2013-08-28 22:33:46 -0500

    Change to use CsQuery instead of Fizzler.

--- PreMailer.Net/PreMailer.Net.Tests/PreMailerTests.cs
+++ PreMailer.Net/PreMailer.Net.Tests/PreMailerTests.cs
@@ -1,59 +1,53 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApprovalTests.Reporters;
 using System.IO;
+using NUnit.Framework;
 
 namespace PreMailer.Net.Tests
 {
-	[TestClass]
-	public class PreMailerTests
+	[TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    public class PreMailerTests
 	{
-		private PreMailer sut;
+		private PreMailer _sut;
 
-		[TestInitialize]
+		[SetUp]
 		public void TestInitialize()
 		{
-			this.sut = new PreMailer();
+			_sut = new PreMailer();
 		}
 
-		[TestMethod]
+		[Test]
 		public void MoveCssInline_RespectExistingStyleElement()
 		{
 			string input = "<html><head><style type=\"text/css\">.test { height: 100px; }</style></head><body><div class=\"test\" style=\"width: 100px;\">test</div></body></html>";
 
-			string premailedOutput = sut.MoveCssInline(input, false);
+			string premailedOutput = _sut.MoveCssInline(input, false);
 
-			Assert.IsTrue(premailedOutput.Contains("<div class=\"test\" style=\"height: 100px;width: 100px;"));
+            Console.WriteLine(premailedOutput);
+            Assert.IsTrue(premailedOutput.Contains("<div class=\"test\" style=\"width: 100px; height: 100px;"));
 		}
 
-		[TestMethod]
+		[Test]
 		public void MoveCssInline_InlineStyleElementTakesPrecedence()
 		{
 			string input = "<html><head><style type=\"text/css\">.test { width: 150px; }</style></head><body><div class=\"test\" style=\"width: 100px;\">test</div></body></html>";
 
-			string premailedOutput = sut.MoveCssInline(input, false);
+			string premailedOutput = _sut.MoveCssInline(input, false);
 
-			Assert.IsTrue(premailedOutput.Contains("<div class=\"test\" style=\"width: 100px;"));
+            Console.WriteLine(premailedOutput);
+			Assert.IsTrue(premailedOutput.Contains("<div class=\"test\" style=\"width: 100px"));
 		}
 
-		[TestMethod, Ignore]
+		[Test]
 		public void ManualIntegrationTest()
 		{
-			string testProjectDirectoryPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-			var directory = new DirectoryInfo(testProjectDirectoryPath);
+			string htmlSource = File.ReadAllText("testmail.html");
 
-			while (!directory.Name.Equals("PreMailer.Net.Tests"))
-			{
-				directory = directory.Parent;
-			}
+			string premailedOutput = _sut.MoveCssInline(htmlSource, false);
+            Console.WriteLine(premailedOutput);
 
-			testProjectDirectoryPath = directory.FullName;
-
-			string htmlSource = File.ReadAllText(String.Join("\\", testProjectDirectoryPath, "testmail.html"));
-
-			string premailedOutput = sut.MoveCssInline(htmlSource, false);
-		}
+            ApprovalTests.Approvals.Verify(premailedOutput);
+        }
 	}
 }
