f8183e90712b0f46e9595159f728f92092fe1f07
Martin H. Normark <m@martinnormark.com>
2013-11-07 22:16:54 +0100

    Bump version to 1.1.3
    Update copyright year

--- PreMailer.Net/GlobalAssemblyInfo.cs
+++ PreMailer.Net/GlobalAssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("PreMailer.Net")]
 [assembly: AssemblyCompany("Milkshake Software")]
-[assembly: AssemblyCopyright("Copyright © Milkshake Software 2011")]
+[assembly: AssemblyCopyright("Copyright © Milkshake Software 2013")]
 
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
 
 #if DEBUG 
 [assembly: AssemblyConfiguration("Debug")] 
