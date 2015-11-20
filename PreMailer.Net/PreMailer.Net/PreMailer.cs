4c61aa1700eedd38995e1484c7995a4ebc54b11f
Sanjeev Manickam <smanickam@gmail.com>
2015-11-20 14:44:15 -0500

    Stupid codemaid re-jigged file!

--- PreMailer.Net/PreMailer.Net/PreMailer.cs
+++ PreMailer.Net/PreMailer.Net/PreMailer.cs
@@ -9,15 +9,15 @@ namespace PreMailer.Net
 {
 	public class PreMailer
 	{
+		private readonly CQ _document;
+		private bool _removeStyleElements;
+		private bool _stripIdAndClassAttributes;
+		private string _ignoreElements;
+		private string _css;
 		private readonly Uri _baseUri;
 		private readonly CssParser _cssParser;
 		private readonly CssSelectorParser _cssSelectorParser;
-		private readonly CQ _document;
 		private readonly List<string> _warnings;
-		private string _css;
-		private string _ignoreElements;
-		private bool _removeStyleElements;
-		private bool _stripIdAndClassAttributes;
 
 		/// <summary>
 		/// Constructor for the PreMailer class
@@ -38,9 +38,7 @@ namespace PreMailer.Net
 		/// </summary>
 		/// <param name="html">The HTML input.</param>
 		/// <param name="removeStyleElements">If set to <c>true</c> the style elements are removed.</param>
-		/// <param name="ignoreElements">
-		/// CSS selector for STYLE elements to ignore (e.g. mobile-specific styles etc.)
-		/// </param>
+		/// <param name="ignoreElements">CSS selector for STYLE elements to ignore (e.g. mobile-specific styles etc.)</param>
 		/// <param name="css">A string containing a style-sheet for inlining.</param>
 		/// <param name="stripIdAndClassAttributes">True to strip ID and class attributes</param>
 		/// <param name="removeComments">True to remove comments, false to leave them intact</param>
@@ -53,13 +51,10 @@ namespace PreMailer.Net
 		/// <summary>
 		/// In-lines the CSS within the HTML given.
 		/// </summary>
-		/// ///
-		/// <param name="baseUri">The base url that will be used to resolve any relative urls</param>
+		/// /// <param name="baseUri">The base url that will be used to resolve any relative urls</param>
 		/// <param name="html">The HTML input.</param>
 		/// <param name="removeStyleElements">If set to <c>true</c> the style elements are removed.</param>
-		/// <param name="ignoreElements">
-		/// CSS selector for STYLE elements to ignore (e.g. mobile-specific styles etc.)
-		/// </param>
+		/// <param name="ignoreElements">CSS selector for STYLE elements to ignore (e.g. mobile-specific styles etc.)</param>
 		/// <param name="css">A string containing a style-sheet for inlining.</param>
 		/// <param name="stripIdAndClassAttributes">True to strip ID and class attributes</param>
 		/// <param name="removeComments">True to remove comments, false to leave them intact</param>
@@ -70,35 +65,10 @@ namespace PreMailer.Net
 		}
 
 		/// <summary>
-		/// Function to add Google analytics tracking tags to the HTML document
-		/// </summary>
-		/// <param name="source">Source tracking tag</param>
-		/// <param name="medium">Medium tracking tag</param>
-		/// <param name="campaign">Campaign tracking tag</param>
-		/// <param name="content">Content tracking tag</param>
-		/// <param name="domain">Optional domain check; if it does not match the URL will be skipped</param>
-		/// <returns>Reference to the instance so you can chain calls.</returns>
-		public PreMailer AddAnalyticsTags(string source, string medium, string campaign, string content, string domain = null)
-		{
-			var tracking = "utm_source=" + source + "&utm_medium=" + medium + "&utm_campaign=" + campaign + "&utm_content=" + content;
-			foreach (var tag in _document["a[href]"])
-			{
-				var href = tag.Attributes["href"];
-				if (href.StartsWith("http", StringComparison.OrdinalIgnoreCase) && (domain == null || DomainMatch(domain, href)))
-				{
-					tag.SetAttribute("href", href + (href.IndexOf("?", StringComparison.Ordinal) >= 0 ? "&" : "?") + tracking);
-				}
-			}
-			return this;
-		}
-
-		/// <summary>
 		/// In-lines the CSS for the current HTML
 		/// </summary>
 		/// <param name="removeStyleElements">If set to <c>true</c> the style elements are removed.</param>
-		/// <param name="ignoreElements">
-		/// CSS selector for STYLE elements to ignore (e.g. mobile-specific styles etc.)
-		/// </param>
+		/// <param name="ignoreElements">CSS selector for STYLE elements to ignore (e.g. mobile-specific styles etc.)</param>
 		/// <param name="css">A string containing a style-sheet for inlining.</param>
 		/// <param name="stripIdAndClassAttributes">True to strip ID and class attributes</param>
 		/// <param name="removeComments">True to remove comments, false to leave them intact</param>
@@ -139,6 +109,125 @@ namespace PreMailer.Net
 			return new InlineResult(html, _warnings);
 		}
 
+		/// <summary>
+		/// Function to add Google analytics tracking tags to the HTML document
+		/// </summary>
+		/// <param name="source">Source tracking tag</param>
+		/// <param name="medium">Medium tracking tag</param>
+		/// <param name="campaign">Campaign tracking tag</param>
+		/// <param name="content">Content tracking tag</param>
+		/// <param name="domain">Optional domain check; if it does not match the URL will be skipped</param>
+		/// <returns>Reference to the instance so you can chain calls.</returns>
+		public PreMailer AddAnalyticsTags(string source, string medium, string campaign, string content, string domain = null)
+		{
+			var tracking = "utm_source=" + source + "&utm_medium=" + medium + "&utm_campaign=" + campaign + "&utm_content=" + content;
+			foreach (var tag in _document["a[href]"])
+			{
+				var href = tag.Attributes["href"];
+				if (href.StartsWith("http", StringComparison.OrdinalIgnoreCase) && (domain == null || DomainMatch(domain, href)))
+				{
+					tag.SetAttribute("href", href + (href.IndexOf("?", StringComparison.Ordinal) >= 0 ? "&" : "?") + tracking);
+				}
+			}
+			return this;
+		}
+
+		/// <summary>
+		/// Function to check if the domain in a URL matches
+		/// </summary>
+		/// <param name="domain">Domain to check</param>
+		/// <param name="url">URL to parse</param>
+		/// <returns>True if the domain matches, false if not</returns>
+		private bool DomainMatch(string domain, string url)
+		{
+			if (url.Contains(@"://"))
+			{
+				url = url.Split(new[] { @"://" }, 2, StringSplitOptions.None)[1];
+			}
+			url = url.Split('/')[0];
+			return string.Compare(domain, url, StringComparison.OrdinalIgnoreCase) == 0;
+		}
+
+		/// <summary>
+		/// Returns the blocks of CSS within the documents supported CSS sources.<para/>
+		/// Blocks are returned in the order they are declared within the document.
+		/// </summary>
+		private IEnumerable<string> GetCssBlocks(IEnumerable<ICssSource> cssSources)
+		{
+			var styleBlocks = new List<string>();
+
+			foreach (var styleSource in cssSources)
+			{
+				styleBlocks.Add(styleSource.GetCss());
+			}
+
+			return styleBlocks;
+		}
+
+		/// <summary>
+		/// Returns a list of CSS sources ('style', 'link' tags etc.) based on the elements given.<para/>
+		/// These will be returned in their order of definition.
+		/// </summary>
+		private IEnumerable<ICssSource> ConvertToStyleSources(CQ nodesWithStyles)
+		{
+			var result = new List<ICssSource>();
+			var nodes = nodesWithStyles;
+			foreach (var node in nodes)
+			{
+				switch (node.NodeName)
+				{
+					case "STYLE":
+						result.Add(new DocumentStyleTagCssSource(node));
+						break;
+
+					case "LINK":
+						result.Add(new LinkTagCssSource(node, _baseUri));
+						break;
+				}
+			}
+
+			if (!String.IsNullOrWhiteSpace(_css))
+			{
+				result.Add(new StringCssSource(_css));
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Returns a collection of CQ 'sytle' nodes that can be used to source CSS content.<para/>
+		/// </summary>
+		private CQ CssSourceNodes()
+		{
+			var elements = _document.Find("style").Not(_ignoreElements).Filter(elem =>
+			{
+				var mediaAttribute = elem.GetAttribute("media");
+
+				return string.IsNullOrWhiteSpace(mediaAttribute) || CssParser.SupportedMediaQueriesRegex.IsMatch(mediaAttribute);
+			});
+			return elements;
+		}
+
+		/// <summary>
+		/// Returns a collection of CQ 'link' nodes that can be used to source CSS content.<para/>
+		/// </summary>
+		private CQ CssLinkNodes()
+		{
+			return _document.Find("link").Not(_ignoreElements)
+					.Filter(e => e.Attributes
+							.Any(a => a.Key.Equals("href", StringComparison.OrdinalIgnoreCase) &&
+							a.Value.EndsWith(".css", StringComparison.OrdinalIgnoreCase)));
+		}
+
+
+		private void RemoveStyleElements(CQ cssSourceNodes)
+		{
+			foreach (var node in cssSourceNodes)
+			{
+				node.Remove();
+			}
+		}
+
 		private static SortedList<string, StyleClass> Join(IEnumerable<string> cssBlocks)
 		{
 			var parser = new CssParser();
@@ -177,81 +266,8 @@ namespace PreMailer.Net
 			return result;
 		}
 
-		/// <summary>
-		/// Returns a list of CSS sources ('style', 'link' tags etc.) based on the elements given.
-		/// <para/>These will be returned in their order of definition.
-		/// </summary>
-		private IEnumerable<ICssSource> ConvertToStyleSources(CQ nodesWithStyles)
-		{
-			var result = new List<ICssSource>();
-			var nodes = nodesWithStyles;
-			foreach (var node in nodes)
-			{
-				switch (node.NodeName)
-				{
-					case "STYLE":
-						result.Add(new DocumentStyleTagCssSource(node));
-						break;
-
-					case "LINK":
-						result.Add(new LinkTagCssSource(node, _baseUri));
-						break;
-				}
-			}
-
-			if (!String.IsNullOrWhiteSpace(_css))
-			{
-				result.Add(new StringCssSource(_css));
-			}
-
-			return result;
-		}
-
-		/// <summary>
-		/// Returns a collection of CQ 'link' nodes that can be used to source CSS content.
-		/// <para/>
-		/// </summary>
-		private CQ CssLinkNodes()
-		{
-			return _document.Find("link").Not(_ignoreElements)
-					.Filter(e => e.Attributes
-							.Any(a => a.Key.Equals("href", StringComparison.OrdinalIgnoreCase) &&
-							a.Value.EndsWith(".css", StringComparison.OrdinalIgnoreCase)));
-		}
-
-		/// <summary>
-		/// Returns a collection of CQ 'sytle' nodes that can be used to source CSS content.
-		/// <para/>
-		/// </summary>
-		private CQ CssSourceNodes()
-		{
-			var elements = _document.Find("style").Not(_ignoreElements).Filter(elem =>
-			{
-				var mediaAttribute = elem.GetAttribute("media");
-
-				return string.IsNullOrWhiteSpace(mediaAttribute) || CssParser.SupportedMediaQueriesRegex.IsMatch(mediaAttribute);
-			});
-			return elements;
-		}
-
-		/// <summary>
-		/// Function to check if the domain in a URL matches
-		/// </summary>
-		/// <param name="domain">Domain to check</param>
-		/// <param name="url">URL to parse</param>
-		/// <returns>True if the domain matches, false if not</returns>
-		private bool DomainMatch(string domain, string url)
-		{
-			if (url.Contains(@"://"))
-			{
-				url = url.Split(new[] { @"://" }, 2, StringSplitOptions.None)[1];
-			}
-			url = url.Split('/')[0];
-			return string.Compare(domain, url, StringComparison.OrdinalIgnoreCase) == 0;
-		}
-
 		private Dictionary<IDomObject, List<StyleClass>> FindElementsWithStyles(
-										SortedList<string, StyleClass> stylesToApply)
+						SortedList<string, StyleClass> stylesToApply)
 		{
 			var result = new Dictionary<IDomObject, List<StyleClass>>();
 
@@ -268,51 +284,6 @@ namespace PreMailer.Net
 			}
 
 			return result;
-		}
-
-		/// <summary>
-		/// Returns the blocks of CSS within the documents supported CSS sources.
-		/// <para/>Blocks are returned in the order they are declared within the document.
-		/// </summary>
-		private IEnumerable<string> GetCssBlocks(IEnumerable<ICssSource> cssSources)
-		{
-			var styleBlocks = new List<string>();
-
-			foreach (var styleSource in cssSources)
-			{
-				styleBlocks.Add(styleSource.GetCss());
-			}
-
-			return styleBlocks;
-		}
-
-		private Dictionary<IDomObject, StyleClass> MergeStyleClasses(
-						Dictionary<IDomObject, List<StyleClass>> styles)
-		{
-			var result = new Dictionary<IDomObject, StyleClass>();
-			var stylesBySpecificity = SortBySpecificity(styles);
-
-			foreach (var elemStyle in stylesBySpecificity)
-			{
-				// CSS Classes are assumed to be sorted by specifity now, so we can just merge these up.
-				var merged = new StyleClass();
-				foreach (var style in elemStyle.Value)
-				{
-					merged.Merge(style, true);
-				}
-
-				result[elemStyle.Key] = merged;
-			}
-
-			return result;
-		}
-
-		private void RemoveStyleElements(CQ cssSourceNodes)
-		{
-			foreach (var node in cssSourceNodes)
-			{
-				node.Remove();
-			}
 		}
 
 		private Dictionary<IDomObject, List<StyleClass>> SortBySpecificity(
@@ -337,6 +308,27 @@ namespace PreMailer.Net
 
 					result[style.Key] = sortedStyles;
 				}
+			}
+
+			return result;
+		}
+
+		private Dictionary<IDomObject, StyleClass> MergeStyleClasses(
+						Dictionary<IDomObject, List<StyleClass>> styles)
+		{
+			var result = new Dictionary<IDomObject, StyleClass>();
+			var stylesBySpecificity = SortBySpecificity(styles);
+
+			foreach (var elemStyle in stylesBySpecificity)
+			{
+				// CSS Classes are assumed to be sorted by specifity now, so we can just merge these up.
+				var merged = new StyleClass();
+				foreach (var style in elemStyle.Value)
+				{
+					merged.Merge(style, true);
+				}
+
+				result[elemStyle.Key] = merged;
 			}
 
 			return result;
