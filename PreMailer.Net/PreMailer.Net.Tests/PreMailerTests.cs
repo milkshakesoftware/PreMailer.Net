0adbb0b3993de6c3af201b12dc4084abd7972a69
Martin H. Normark <m@martinnormark.com>
2016-08-02 21:27:15 +0200

    Add test case for #54
    Closes #54

--- PreMailer.Net/PreMailer.Net.Tests/PreMailerTests.cs
+++ PreMailer.Net/PreMailer.Net.Tests/PreMailerTests.cs
@@ -445,5 +445,15 @@ namespace PreMailer.Net.Tests
 
 			Assert.IsTrue(premailedOutput.Html.StartsWith("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\">"));
 		}
+
+		[TestMethod]
+		public void MoveCSSInline_PreservesXMLNamespace()
+		{
+			string input = "<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\"><head></head><body></body></html>";
+
+			var premailedOutput = PreMailer.MoveCssInline(input, false);
+
+			Assert.IsTrue(premailedOutput.Html.StartsWith("<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\">"));
+		}
 	}
 }
