f8edf8d40a009819fa6a5f54effeb46b3a2be16b
Martin H. Normark <m@martinnormark.com>
2016-11-07 22:06:50 +0100

    Bump version to 1.5.5 and update release notes

--- PreMailer.Net/GlobalAssemblyInfo.cs
+++ PreMailer.Net/GlobalAssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Milkshake Software")]
 [assembly: AssemblyCopyright("Copyright © Milkshake Software 2016")]
 
-[assembly: AssemblyVersion("1.5.4.0")]
-[assembly: AssemblyFileVersion("1.5.4.0")]
+[assembly: AssemblyVersion("1.5.5.0")]
+[assembly: AssemblyFileVersion("1.5.5.0")]
 
 #if DEBUG 
 [assembly: AssemblyConfiguration("Debug")] 
