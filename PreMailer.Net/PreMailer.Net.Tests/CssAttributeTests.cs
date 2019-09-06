f511c89dcc60cce74a2d26c7ea099169bdb1367e
Martin H. Normark <m@martinnormark.com>
2019-09-06 22:43:56 +0200

    Format whitespace

--- PreMailer.Net/PreMailer.Net.Tests/CssAttributeTests.cs
+++ PreMailer.Net/PreMailer.Net.Tests/CssAttributeTests.cs
@@ -2,70 +2,70 @@
 
 namespace PreMailer.Net.Tests
 {
-    [TestClass]
-    public class CssAttributeTests
-    {
-        [TestMethod]
-        public void StandandUnimportantRule_ReturnsAttribute()
-        {
-            var attribute = CssAttribute.FromRule("color: red");
+	[TestClass]
+	public class CssAttributeTests
+	{
+		[TestMethod]
+		public void StandandUnimportantRule_ReturnsAttribute()
+		{
+			var attribute = CssAttribute.FromRule("color: red");
 
-            Assert.AreEqual("color", attribute.Style);
-            Assert.AreEqual("red", attribute.Value);
-        }
+			Assert.AreEqual("color", attribute.Style);
+			Assert.AreEqual("red", attribute.Value);
+		}
 
-        [TestMethod]
-        public void MixedCaseRuleValue_RetainsCasing()
-        {
-            var attribute = CssAttribute.FromRule(" color: rED");
+		[TestMethod]
+		public void MixedCaseRuleValue_RetainsCasing()
+		{
+			var attribute = CssAttribute.FromRule(" color: rED");
 
-            Assert.AreEqual("color", attribute.Style);
-            Assert.AreEqual("rED", attribute.Value);
-        }
+			Assert.AreEqual("color", attribute.Style);
+			Assert.AreEqual("rED", attribute.Value);
+		}
 
-        [TestMethod]
-        public void MixedCaseRule_RetainsCasing()
-        {
-            var attribute = CssAttribute.FromRule("Margin-bottom: 10px");
+		[TestMethod]
+		public void MixedCaseRule_RetainsCasing()
+		{
+			var attribute = CssAttribute.FromRule("Margin-bottom: 10px");
 
-            Assert.AreEqual("Margin-bottom", attribute.Style);
-            Assert.AreEqual("10px", attribute.Value);
-        }
+			Assert.AreEqual("Margin-bottom", attribute.Style);
+			Assert.AreEqual("10px", attribute.Value);
+		}
 
-        [TestMethod]
-        public void ImportantRule_ReturnsImportantAttribute()
-        {
-            var attribute = CssAttribute.FromRule("color: red !important");
+		[TestMethod]
+		public void ImportantRule_ReturnsImportantAttribute()
+		{
+			var attribute = CssAttribute.FromRule("color: red !important");
 
-            Assert.AreEqual("color", attribute.Style);
-            Assert.AreEqual("red", attribute.Value);
-            Assert.IsTrue(attribute.Important);
-        }
+			Assert.AreEqual("color", attribute.Style);
+			Assert.AreEqual("red", attribute.Value);
+			Assert.IsTrue(attribute.Important);
+		}
 
-        [TestMethod]
-        public void ImportantRule_ReturnsValidCssWithoutWhitespaces()
-        {
-            var attribute = CssAttribute.FromRule("color:red!important");
+		[TestMethod]
+		public void ImportantRule_ReturnsValidCssWithoutWhitespaces()
+		{
+			var attribute = CssAttribute.FromRule("color:red!important");
 
-            Assert.AreEqual("color", attribute.Style);
-            Assert.AreEqual("red", attribute.Value);
-            Assert.IsTrue(attribute.Important);
-        }
+			Assert.AreEqual("color", attribute.Style);
+			Assert.AreEqual("red", attribute.Value);
+			Assert.IsTrue(attribute.Important);
+		}
 
-        [TestMethod]
-        public void NonRule_ReturnsNull()
-        {
-            var attribute = CssAttribute.FromRule(" ");
+		[TestMethod]
+		public void NonRule_ReturnsNull()
+		{
+			var attribute = CssAttribute.FromRule(" ");
 
-            Assert.IsNull(attribute);
-        }
+			Assert.IsNull(attribute);
+		}
 
-        [TestMethod]
-        public void FromRule_OnlySplitsTheRuleAtTheFirstColonToSupportUrls()
-        {
-            var attribute = CssAttribute.FromRule("background: url('http://my.web.site.com/Content/email/content.png') repeat-y");
+		[TestMethod]
+		public void FromRule_OnlySplitsTheRuleAtTheFirstColonToSupportUrls()
+		{
+			var attribute = CssAttribute.FromRule("background: url('http://my.web.site.com/Content/email/content.png') repeat-y");
 
-            Assert.AreEqual("url('http://my.web.site.com/Content/email/content.png') repeat-y", attribute.Value);
-        }
-    }
+			Assert.AreEqual("url('http://my.web.site.com/Content/email/content.png') repeat-y", attribute.Value);
+		}
+	}
 }
