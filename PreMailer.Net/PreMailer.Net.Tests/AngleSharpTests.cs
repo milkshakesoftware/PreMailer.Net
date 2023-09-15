d6ad7af2a68a52048a9e39bbacae78089d251ac9
Jason Gallavin <jasongallavin@gmail.com>
2023-09-14 23:43:15 -0400

    Add test to indicate AngleSharp is the issue with the html entity encoding
    Also found out turning on IsNotConsumingCharacterReferences does not fix the issue and causes a strange result:
    
    Expected: <html><head></head><body><div>&lt;&amp;&gt;&nbsp;&copy;</div></body></html>
    Actual:   <html><head></head><body><div>&amp;lt;&amp;amp;&amp;gt;&amp;nbsp;&amp;copy;</div></body></html>

---
+++ PreMailer.Net/PreMailer.Net.Tests/AngleSharpTests.cs
@@ -0,0 +1,21 @@
+﻿using AngleSharp;
+using AngleSharp.Html.Parser;
+using Xunit;
+
+namespace PreMailer.Net.Tests;
+
+public class AngleSharpTests
+{
+	[Fact]
+	public void HtmlDocument_ToHtml_ShouldNotEffectHtmlEntities()
+	{
+		string htmlEncoded = "&lt;&amp;&gt;&nbsp;&copy;";
+		string input = $"<html><head></head><body><div>{htmlEncoded}</div></body></html>";
+		var document = new HtmlParser(new HtmlParserOptions()
+		{
+			IsNotConsumingCharacterReferences = true
+		}).ParseDocument(input);
+		var output = document.ToHtml();
+		Assert.Equal<object>(input, output); // use object to get full string output
+	}
+}
