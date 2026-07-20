8b92b68425d9f49dfc90a7ff9a2ca0783e40fd03
Sorry <278210653+Prana111025@users.noreply.github.com>
2026-07-21 02:22:05 +0600

    Delete PreMailer.Net directory

--- PreMailer.Net/PreMailer.Net/ICssSelectorParser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PreMailer.Net
-{
-	public interface ICssSelectorParser
-	{
-		int GetSelectorSpecificity(string selector);
-		bool IsPseudoElement(string selector);
-		bool IsPseudoClass(string selector);
-	}
-}
